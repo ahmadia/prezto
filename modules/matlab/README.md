43c50f3a876ac9d367c07694c0b51c8621a82616
Aron Ahmadia <aron@ahmadia.net>
2014-06-25 11:38:42 -0400

    add matlab

---
+++ modules/matlab/README.md
@@ -0,0 +1,9 @@
+MATLAB
+======
+
+Enables MATLAB installation
+
+Aliases
+-------
+
+  - `mb` is short for `matlab`.
---
+++ modules/matlab/init.zsh
@@ -0,0 +1,12 @@
+#
+# Enables MATLAB
+#
+
+export MATLAB=/Applications/MATLAB_R2014a.app
+path=("$MATLAB/bin" $path)
+
+#
+# Aliases
+#
+
+alias mb='matlab'
